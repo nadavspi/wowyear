56b7f7288dbc110b642ea1410a53b0fe2213d95d
Nadav Spiegelman <me@nadav.name>
2013-11-11 17:48:09 -0500

    preventDefault for countdown click

--- js/site.js
+++ js/site.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
   var now   = new Date();
   var diff  = date.getTime()/1000 - now.getTime()/1000;
 
-  var clock = $('.countdown').FlipClock(diff, {
+  var clock = $('.countdown-js').FlipClock(diff, {
       clockFace: 'DailyCounter',
       countdown: true
     });
+
+  $('.countdown-js').on("click", "a", function(event) { 
+    event.preventDefault();
+  });
 });
