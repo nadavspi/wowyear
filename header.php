4745c644cbc700fb3cf1483927b6ed3f0443054a
Nadav Spiegelman <me@nadav.name>
2013-12-14 13:18:31 -0500

    Menu stuff

--- header.php
+++ header.php
@@ -23,15 +23,18 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
+
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<h3 class="menu-toggle"><?php _e( 'Menu', 'wowyear' ); ?></h3>
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wowyear' ); ?></a>
+
+		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+	</nav><!-- #site-navigation -->
+
 	<header id="masthead" class="site-header" role="banner">
 		<img src="<?php bloginfo('template_directory'); ?>/images/wowyear-banner.jpg" alt="Wow Year 2014" class="site-banner">
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'wowyear' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wowyear' ); ?></a>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+	
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
