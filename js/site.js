27438358e9189aa063e97645f0ef0cfc2520f078
Nadav Spiegelman <me@nadav.name>
2013-12-29 21:00:25 -0500

    Ch-ch-ch-changes

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $("#giga-js").fitText(0.5);
   $("#mega-js").fitText(0.9, { minFontSize: '40px'});
 
-  var date  = new Date(Date.UTC(2014, 0, 13, 12, 0, 0));
+  var date  = new Date(Date.UTC(2014, 0, 13, 18, 0, 0));
   var now   = new Date();
   var diff  = date.getTime()/1000 - now.getTime()/1000;
 
